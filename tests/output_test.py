e45135e79a949b99bb8401b098f887629f6cd480
Paweł <pawel1116574@gmail.com>
2022-08-23 01:14:53 +0200

    add tests for new format

--- tests/output_test.py
+++ tests/output_test.py
@@ -10,12 +10,62 @@ class Args:
         self.text = text
         self.out = out
         self.debug_tokenizer = False
+        self.debug_pattern = False
+        self.out_file = False
         self.verbosity = 3
 
 
 def test_json():
     args = Args('Z powodu awarii metro kursuje w pętli Słodowiec-Marymont. Uruchomiono linię zastępczą ZM1', 'json')
-    assert __main__.main(args) == {'status': 'Loop', 'affected': [{'name': 'Marymont', 'id': 'A19'}, {'name': 'Słodowiec', 'id': 'A20'}], 'reason': 'awari', 'replacement_service': {'exists': True, 'name': 'ZM1', 'by_extension': False}}
+    assert __main__.main(args) == {'conditions': [{'status': 'Loop', 'affected': [{'name': 'Marymont', 'id': 'A19'}, {'name': 'Słodowiec', 'id': 'A20'}]}], 'reason': 'awari', 'replacement_service': {'exists': True, 'name': 'ZM1', 'by_extension': False}}
+
+
+def test_json_multiple_conditions():
+    args = Args('Z powodu awarii metro M1 kursuje w pętli Słodowiec-Marymont, a M2 w pętli Trocka-Dw.Wileński. Uruchomiono linię zastępczą ZM1', 'json')
+    assert __main__.main(args) == {
+        "conditions": [
+            {
+                "status": "Loop",
+                "affected": [
+                    {
+                        "name": "Marymont",
+                        "id": "A19"
+                    },
+                    {
+                        "name": "S\u0142odowiec",
+                        "id": "A20"
+                    }
+                ]
+            },
+            {
+                "status": "Loop",
+                "affected": [
+                    {
+                        "name": "Dworzec Wile\u0144ski",
+                        "id": "C15"
+                    },
+                    {
+                        "name": "Szwedzka",
+                        "id": "C16"
+                    },
+                    {
+                        "name": "Targ\u00f3wek Mieszkaniowy",
+                        "id": "C17"
+                    },
+                    {
+                        "name": "Trocka",
+                        "id": "C18"
+                    }
+                ]
+            }
+        ],
+        "reason": "awari",
+        "replacement_service": {
+            "exists": True,
+            "name": "ZM1",
+            "by_extension": False
+        }
+    }
 
 
 class TestHistorical:
@@ -31,4 +81,4 @@ class TestHistorical:
     def test_18(self):
         result = __main__.main(Args(self.get_history(18), 'json'))
         # print(result)
-        assert result == {'status': 'Double_Loop', 'affected': [[{'name': 'Rondo ONZ', 'id': 'C10'}, {'name': 'Świętokrzyska', 'id': 'C11'}, {'name': 'Nowy Świat-Uniwersytet', 'id': 'C12'}, {'name': 'Centrum Nauki Kopernik', 'id': 'C13'}, {'name': 'Stadion Narodowy', 'id': 'C14'}, {'name': 'Dworzec Wileński', 'id': 'C15'}, {'name': 'Szwedzka', 'id': 'C16'}, {'name': 'Targówek Mieszkaniowy', 'id': 'C17'}, {'name': 'Trocka', 'id': 'C18'}], [{'name': 'Księcia Janusza', 'id': 'C6'}, {'name': 'Młynów', 'id': 'C7'}, {'name': 'Płocka', 'id': 'C8'}, {'name': 'Rondo Daszyńskiego', 'id': 'C9'}]], 'reason': 'zdarzenia', 'replacement_service': {'exists': True, 'name': 'by_extension', 'by_extension': False}}
+        assert result == {'conditions': [{'status': 'Double_Loop', 'affected': [[{'name': 'Rondo ONZ', 'id': 'C10'}, {'name': 'Świętokrzyska', 'id': 'C11'}, {'name': 'Nowy Świat-Uniwersytet', 'id': 'C12'}, {'name': 'Centrum Nauki Kopernik', 'id': 'C13'}, {'name': 'Stadion Narodowy', 'id': 'C14'}, {'name': 'Dworzec Wileński', 'id': 'C15'}, {'name': 'Szwedzka', 'id': 'C16'}, {'name': 'Targówek Mieszkaniowy', 'id': 'C17'}, {'name': 'Trocka', 'id': 'C18'}], [{'name': 'Księcia Janusza', 'id': 'C6'}, {'name': 'Młynów', 'id': 'C7'}, {'name': 'Płocka', 'id': 'C8'}, {'name': 'Rondo Daszyńskiego', 'id': 'C9'}]]}], 'reason': 'zdarzenia', 'replacement_service': {'exists': True, 'name': 'by_extension', 'by_extension': False}}
