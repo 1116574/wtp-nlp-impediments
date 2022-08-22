546b2ff59af678d92a8e130ecc22ba7ba30d345e
Paweł <pawel1116574@gmail.com>
2022-08-22 05:01:45 +0200

    fix for tests broken after tweaking data

--- tests/historical_test.py
+++ tests/historical_test.py
@@ -4,6 +4,7 @@ from wtp_nlp.nlp.language_processor import language_processor
 from wtp_nlp.data.metro_stations import *
 from wtp_nlp.data.tokens import *
 from wtp_nlp.data import status
+from wtp_nlp.nlp.highlihter import tokenizer
 
 from bs4 import BeautifulSoup
 
@@ -19,8 +20,9 @@ class TestHistorical:
 
         # self.indexes = [17, 19, 20, 35, 66]
 
-    def test_17(self):
+    def test_17_tokenizer(self):
         result = language_processor(self.get_history(17))
+        # result = tokenizer(self.get_history(17))
         assert result == [
             {
                 'name': 'relation',
@@ -33,24 +35,43 @@ class TestHistorical:
                 'processed_to': NotImplemented
             },
             {
+                'name': 'reason',
+                'matched': [Reason('technicznych')],
+                'processed_to': (status.Reason, Reason('technicznych'))
+            },
+            {
+                'name': 'reason',
+                'matched': [Reason('technicznych')],
+                'processed_to': (status.Reason, Reason('technicznych'))
+            },
+            {
                 'name': 'shortened_service',
                 'matched': [Shortened_Service, 0, On, 2, C6, 1, Between, 1, C17],
                 'processed_to': (status.Loop, [C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16, C17])
             },
             {
-                'matched': [Not_Functioning_Station, 1, C18],
                 'name': 'station_closed',
+                'matched': [Not_Functioning_Station, 1, C18],
+                'processed_to': NotImplemented},
+            {
+                'name': 'replacement_service_by_extension',
+                'matched': [Detour_By_Extension],
                 'processed_to': NotImplemented
             },
+            {
+                'name': 'replacement_service_by_extension',
+                'matched': [Detour_By_Extension],
+                'processed_to': NotImplemented
+            }
         ]
 
     def test_19_facility(self):
         result = language_processor(self.get_history(19))
-        assert result[0]['processed_to'] == (status.Facilities, C8)
+        assert result[2]['processed_to'] == (status.Facilities, C8)
 
     def test_20_loop(self):
         result = language_processor(self.get_history(20))
-        assert result[2]['processed_to'] == (status.Loop, [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A13, A14, A15, A17, A18, A19, A20])
+        assert result[4]['processed_to'] == (status.Loop, [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A13, A14, A15, A17, A18, A19, A20])
 
     def test_35_loop(self):
         result = language_processor(self.get_history(35))
@@ -58,15 +79,15 @@ class TestHistorical:
 
     def test_66_loop(self):
         result = language_processor(self.get_history(66))
-        assert result[3]['processed_to'] == (status.Loop, [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A13, A14, A15, A17, A18, A19, A20])
+        assert result[5]['processed_to'] == (status.Loop, [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A13, A14, A15, A17, A18, A19, A20])
 
     def test_73_loop(self):
         result = language_processor(self.get_history(73))
-        assert result[4]['processed_to'] == (status.Loop, [A7, A8, A9, A10, A11, A13, A14, A15, A17, A18, A19, A20, A21, A22, A23])   
+        assert result[6]['processed_to'] == (status.Loop, [A7, A8, A9, A10, A11, A13, A14, A15, A17, A18, A19, A20, A21, A22, A23])   
 
     def test_3_degrade(self):
         result = language_processor(self.get_history(3))
-        assert result[4]['processed_to'][0] == status.Loop
-        assert result[5]['processed_to'] == (status.Degraded, TOKEN_M2)
+        assert result[-2]['processed_to'][0] == status.Loop
+        assert result[-1]['processed_to'] == (status.Degraded, TOKEN_M2)
 
 # TestHistorical.test_17()
