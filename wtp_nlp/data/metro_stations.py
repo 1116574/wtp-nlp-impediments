29d826739f312db705352f58a9cf3ebd3daf0e7a
Paweł <pawel1116574@gmail.com>
2022-09-09 01:25:43 +0200

    add non-unicode names for stations

--- wtp_nlp/data/metro_stations.py
+++ wtp_nlp/data/metro_stations.py
@@ -18,7 +18,7 @@
 'C18' 'Trocka'
 
 """
-
+import unicodedata
 from dataclasses import dataclass
 
 @dataclass
@@ -32,11 +32,26 @@ class Station:
 
     __match_args__ = ('id', 'name', 'forms', 'turn_around')
 
+    @classmethod
+    def _ascii(cls, unicode_rich_text: str) -> str:
+        text = unicodedata.normalize('NFD', unicode_rich_text)
+        new_text = ''
+        for char in text:
+            if ord(char) < 127:  # 127 is highest ascii character
+                new_text += char
+        
+        return new_text
+
+
     def __repr__(self):
         return f'< {self.id}/{self.name} >'
 
     def __iter__(self):
-        return (['Metro ' + self.name, self.name] + self.forms).__iter__()
+        all_forms = ['Metro ' + self.name, self.name] + self.forms
+        for form in all_forms[:]:  # The list[:] is to create a copy of an object, since the original will be appended to in the same loop
+            all_forms.append(self._ascii(form))
+        
+        return all_forms.__iter__()
 
     def __hash__(self) -> int:
         return (self.name + ''.join(self.forms)).__hash__()
@@ -59,8 +74,8 @@ A11 = Station('A11', 'Politechnika', [], '7006m', True)
 A13 = Station('A13', 'Centrum', [], '7013m', True)
 A14 = Station('A14', 'Świętokrzyska', ['Świętokrzyskiej'], '7014m')
 A15 = Station('A15', 'Ratusz Arsenał', ['Ratusz-Arsenał', 'Ratuszu Arsenał'], '7099m')
-A17 = Station('A17', 'Dworzec Gdański', ['Dw.Gdański', 'Dw. Gdański', 'Gdański'], '7019m', True)
-A18 = Station('A18', 'Plac Wilsona', ['Pl.Wilsona', 'Pl. Wilsona', 'pl.Wilsona', 'plac Wilsona'], '6003m', True)
+A17 = Station('A17', 'Dworzec Gdański', ['Dw.Gdański', 'Dw. Gdański', 'Gdański', 'dworzec gdański'], '7019m', True)
+A18 = Station('A18', 'Plac Wilsona', ['Pl.Wilsona', 'Pl. Wilsona', 'pl.Wilsona', 'plac Wilsona', 'plac wilsona'], '6003m', True)
 A19 = Station('A19', 'Marymont', ['Marymontu'], '6005m')
 A20 = Station('A20', 'Słodowiec', ['Słodowca'], '6006m', True)
 A21 = Station('A21', 'Stare Bielany', ['Starych Bielan'], '6052m')
